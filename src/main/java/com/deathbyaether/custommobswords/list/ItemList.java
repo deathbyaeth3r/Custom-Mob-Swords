b655f07bf7a51a6801aafa13d5998ecb1616b994
deathbyaeth3r <pottsrich3@gmail.com>
2021-11-09 18:31:16 -0500

    Fixed sword registries

--- src/main/java/com/deathbyaether/custommobswords/list/ItemList.java
+++ src/main/java/com/deathbyaether/custommobswords/list/ItemList.java
@@ -1,40 +1,50 @@
 package com.deathbyaether.custommobswords.list;
 
 import com.deathbyaether.custommobswords.Main;
+import com.deathbyaether.custommobswords.util.enums.ModItemTier;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemList
 {
-	
+	public static final ItemGroup SwordTab = Main.SWORD_TAB;
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<Item>(ForgeRegistries.ITEMS, Main.MOD_ID);
 
+	
+	
+	public static final RegistryObject<Item> SWORD_GEMSTONE = ITEMS.register("sword_gemstone", () -> new Item(new Item.Properties().group(SwordTab)));
+	
 	//passive + neutral mobs 
-	public static final RegistryObject<Item> OCELOT_SWORD = ITEMS.register("ocelot_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> CHICKEN_SWORD = ITEMS.register("chicken_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> PIG_SWORD = ITEMS.register("pig_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> SQUID_SWORD = ITEMS.register("squid_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> PUFFERFISH_SWORD = ITEMS.register("pufferfish_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> RABBIT_SWORD = ITEMS.register("rabbit_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
+	public static final RegistryObject<SwordItem> OCELOT_SWORD = ITEMS.register("ocelot_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> CHICKEN_SWORD = ITEMS.register("chicken_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> PIG_SWORD = ITEMS.register("pig_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> SQUID_SWORD = ITEMS.register("squid_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> PUFFERFISH_SWORD = ITEMS.register("pufferfish_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> RABBIT_SWORD = ITEMS.register("rabbit_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
 	//aggressive mobs
-	public static final RegistryObject<Item> GHAST_SWORD = ITEMS.register("ghast_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> ZOMBIE_SWORD = ITEMS.register("zombie_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> SKELETON_SWORD = ITEMS.register("skeleton_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> SHULKER_SWORD = ITEMS.register("shulker_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> CREEPER_SWORD = ITEMS.register("creeper_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> ENDERMAN_SWORD = ITEMS.register("enderman_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
+	public static final RegistryObject<SwordItem> GHAST_SWORD = ITEMS.register("ghast_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> ZOMBIE_SWORD = ITEMS.register("zombie_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> SKELETON_SWORD = ITEMS.register("skeleton_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> SHULKER_SWORD = ITEMS.register("shulker_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> CREEPER_SWORD = ITEMS.register("creeper_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> ENDERMAN_SWORD = ITEMS.register("enderman_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
 
 	//boss mobs
-	public static final RegistryObject<Item> WITHER_SWORD = ITEMS.register("wither_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> ENDERDRAGON_SWORD = ITEMS.register("enderdragon_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
+	public static final RegistryObject<SwordItem> WITHER_SWORD = ITEMS.register("wither_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> ENDERDRAGON_SWORD = ITEMS.register("enderdragon_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
 	//special mobs
-	public static final RegistryObject<Item> BEE_SWORD = ITEMS.register("bee_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
-	public static final RegistryObject<Item> DIAMONDCHICKEN_SWORD = ITEMS.register("diamondchicken_sword", () -> new Item(new Item.Properties().group(Main.SWORD_TAB)));
+	public static final RegistryObject<SwordItem> BEE_SWORD = ITEMS.register("bee_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<SwordItem> DIAMONDCHICKEN_SWORD = ITEMS.register("diamondchicken_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	
+	
+	
 	
 }
---
+++ src/main/java/com/deathbyaether/custommobswords/util/enums/ModItemTier.java
@@ -0,0 +1,62 @@
+package com.deathbyaether.custommobswords.util.enums;
+
+import java.util.function.Supplier;
+
+import com.deathbyaether.custommobswords.list.ItemList;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+public enum ModItemTier implements IItemTier {
+
+    SWORD_GEM(3, 1571, 7.0F, 7.0F, 12, () -> {
+        return Ingredient.fromItems(ItemList.SWORD_GEMSTONE.get());
+    });
+
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
