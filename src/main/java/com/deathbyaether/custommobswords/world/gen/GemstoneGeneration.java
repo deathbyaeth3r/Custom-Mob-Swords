9fae2688eb338721d9cf12ef76d7446ba320fa5a
deathbyaeth3r <pottsrich3@gmail.com>
2021-11-09 22:34:13 -0500

    Ore parameter adjustments

--- src/main/java/com/deathbyaether/custommobswords/world/gen/GemstoneGeneration.java
+++ src/main/java/com/deathbyaether/custommobswords/world/gen/GemstoneGeneration.java
@@ -18,7 +18,7 @@ public class GemstoneGeneration {
 					Feature.ORE
 							.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
 									BlockList.GEMSTONE_ORE.get().getDefaultState(), 10))
-							.withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(20, 5, 0, 128))));
+							.withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 16, 8, 70))));
 		}
 	}
 	
