0a6f8e90fa10560ce231d267fa16f6f9172e8f38
deathbyaeth3r <pottsrich3@gmail.com>
2022-03-07 12:18:30 -0500

    Models, lang, edits

--- src/main/java/com/deathbyaether/custommobswords/list/ItemList.java
+++ src/main/java/com/deathbyaether/custommobswords/list/ItemList.java
@@ -29,16 +29,14 @@ public class ItemList
 	public static final RegistryObject<Item> SWORD_GEMSTONE = ITEMS.register("sword_gemstone", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 	
 	//passive + neutral mobs 
-	public static final RegistryObject<SwordItem> PIG_SWORD = ITEMS.register("pig_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	public static final RegistryObject<SquidSwordItem> SQUID_SWORD = ITEMS.register("squid_sword", () -> new SquidSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	public static final RegistryObject<PufferfishSwordItem> PUFFERFISH_SWORD = ITEMS.register("pufferfish_sword", () -> new PufferfishSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
-	public static final RegistryObject<SwordItem> RABBIT_SWORD = ITEMS.register("rabbit_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	
 	
 	//aggressive mobs
 	public static final RegistryObject<GhastSwordItem> GHAST_SWORD = ITEMS.register("ghast_sword", () -> new GhastSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
-	public static final RegistryObject<SwordItem> ZOMBIE_SWORD = ITEMS.register("zombie_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	public static final RegistryObject<SkeletonSwordItem> SKELETON_SWORD = ITEMS.register("skeleton_sword", () -> new SkeletonSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
-	public static final RegistryObject<SwordItem> SHULKER_SWORD = ITEMS.register("shulker_sword", () -> new SwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	//public static final RegistryObject<ShulkerSwordItem> SHULKER_SWORD = ITEMS.register("shulker_sword", () -> new ShulkerSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	public static final RegistryObject<CreeperSwordItem> CREEPER_SWORD = ITEMS.register("creeper_sword", () -> new CreeperSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	public static final RegistryObject<EndermanSwordItem> ENDERMAN_SWORD = ITEMS.register("enderman_sword", () -> new EndermanSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
@@ -48,7 +46,7 @@ public class ItemList
 	public static final RegistryObject<EnderdragonSwordItem> ENDERDRAGON_SWORD = ITEMS.register("enderdragon_sword", () -> new EnderdragonSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
 	//special mobs
-	public static final RegistryObject<SwordItem> BEE_SWORD = ITEMS.register("bee_sword", () -> new BeeSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
+	public static final RegistryObject<BeeSwordItem> BEE_SWORD = ITEMS.register("bee_sword", () -> new BeeSwordItem(ModItemTier.SWORD_GEM, 0, 0, new Item.Properties().group(SwordTab)));
 	
 	
 	
