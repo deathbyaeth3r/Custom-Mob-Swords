581dc8428ba8aff0cd21cb26c122c3d8873237ce
pottsrich3 <pottsrich3@gmail.com>
2025-01-31 18:38:48 -0500

    Revert "Revert "Revert "Revert "Guys- Its been almost 3 years... I'm Back!!!""""
    This reverts commit 40ff07a9c27777609ee04be4c4bdcd4fa5f0ddd5.

--- src/main/java/com/deathbyaether/custommobswords/objects/entities/DragonForceEntity.java
+++ src/main/java/com/deathbyaether/custommobswords/objects/entities/DragonForceEntity.java
@@ -27,7 +27,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class DragonForceEntity extends ProjectileItemEntity {
-	public int explosionPower = 5;
+	public int explosionPower = 10;
+	
 	private Entity target;
 	@Nullable
 	private Direction direction;
