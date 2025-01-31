001f8472474cef2fe926a673375f10f84eb7e39f
pottsrich3 <pottsrich3@gmail.com>
2025-01-31 18:38:22 -0500

    Revert "Revert "Guys- Its been almost 3 years... I'm Back!!!""
    This reverts commit 9bbad76b5aa6dde3535a866742cc499c7403e2c1.

--- src/main/java/com/deathbyaether/custommobswords/Main.java
+++ src/main/java/com/deathbyaether/custommobswords/Main.java
@@ -12,6 +12,7 @@ import com.deathbyaether.custommobswords.list.ParticleList;
 import com.deathbyaether.custommobswords.objects.entities.renderer.BeeStingProjectileRenderer;
 import com.deathbyaether.custommobswords.objects.entities.renderer.CreeperProjectileRenderer;
 import com.deathbyaether.custommobswords.objects.entities.renderer.DragonForceRenderer;
+import com.deathbyaether.custommobswords.objects.entities.renderer.SpiderWebProjectileRenderer;
 import com.deathbyaether.custommobswords.objects.entities.renderer.SquidInkRenderer;
 import com.deathbyaether.custommobswords.world.gen.GemstoneGeneration;
 
@@ -105,6 +106,7 @@ public class Main
 		RenderingRegistry.registerEntityRenderingHandler(EntityList.DRAGONFORCE_PROJETILE.get(), DragonForceRenderer::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityList.SQUIDINK_PROJETILE.get(), SquidInkRenderer::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityList.BEESTING_PROJETILE.get(), BeeStingProjectileRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(EntityList.SPIDERWEB_PROJETILE.get(), SpiderWebProjectileRenderer::new);
 	}
 	
 	
